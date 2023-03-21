967c549facf05fd6acf754dfe13164825ff46be1
Igor Solodovnik <igsolodovnik@yandex.ru>
2023-03-21 18:00:31 +0700

    YU-01 настроил визуальное отображение

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
-
+    <main-component />
   </div>
 </template>
 
 <script>
+import MainComponent from './components/MainComponent.vue';
 
 export default {
   name: 'App',
   components: {
-
+    MainComponent,
   }
 }
 </script>
 
 <style>
-#app {
-
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.3rem;
+  margin: 10px;
 }
 </style>
---
+++ src/components/AvailableItems.vue
@@ -0,0 +1,93 @@
+<template>
+   <div class="available">
+      <h3 class="available__title">
+         Вещи на выбор
+      </h3>
+      <div v-for="item of allAvailableItems" :key="item.id" class="available__items">
+         {{ item.name }}
+      </div>
+   </div>
+</template>
+
+<script>
+   export default {
+      data() {
+         return {
+            allAvailableItems: [
+      {
+         "id": 11,
+         "name": "Jacket 1"
+      },
+      {
+         "id": 12,
+         "name": "Jacket 2"
+      },
+      {
+         "id": 13,
+         "name": "Jacket 3"
+      },
+      {
+         "id": 14,
+         "name": "Jacket 4"
+      },
+      {
+         "id": 15,
+         "name": "Hoodie 1"
+      },
+      {
+         "id": 16,
+         "name": "Hoodie 2"
+      },
+      {
+         "id": 17,
+         "name": "Hoodie 3"
+      },
+      {
+         "id": 18,
+         "name": "Hoodie 4"
+      }
+]
+         }
+      }
+   }
+</script>
+
+<style lang="scss" scoped>
+.available {
+   width: calc(50% - 30px);
+   height: 50vh;
+   border: 2px solid black;
+   padding: 10px;
+   display: flex;
+   flex-wrap: wrap;
+   align-content: flex-start;
+   gap: 10px;
+   position: relative;
+   background: white;
+   &__items { 
+      width: 100px;
+      height: 100px;
+      border: 2px solid black;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      transition: all .4s ease;
+      &:hover {
+         border: 2px solid orange;
+         cursor: pointer;
+      }
+   }
+   &__title {
+      position: absolute;
+      top: -50px;
+      left: 50%;
+      transform: translate(-50%, 0);
+      font-size: 18px;
+      background: orange;
+      color: rgb(120, 78, 0);
+      padding: 5px 10px;
+      border-radius: 5px 5px 0 0;
+      z-index: -1;
+   }
+}
+</style>
